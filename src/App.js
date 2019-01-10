8df288a600124c0e48282c5bfa96e1f3f08fbdd9
Mario Fink <mario.fink@goodyear.com>
2019-01-10 15:15:21 +0100

    add simple counter with React state management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+  increase = () => {
+    this.setState({
+      counter: this.state.counter + 1 // as numbers are a primitive type, no need for copying
+    });
+  };
+  decrease = () => {
+    this.setState({
+      counter: this.state.counter - 1 // as numbers are a primitive type, no need for copying
+    });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.counter}</p>
+          <button onClick={this.increase}>Increase</button>
+          <button onClick={this.decrease}>Decrease</button>
         </header>
       </div>
     );
