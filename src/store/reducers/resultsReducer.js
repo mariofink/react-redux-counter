442b3a70deb6c5c900853647da971096c05d1dd3
Mario Fink <mario.fink@goodyear.com>
2020-10-20 16:36:54 +0200

    use direct state mutation
    https://redux-toolkit.js.org/api/createReducer#direct-state-mutation

--- src/store/reducers/resultsReducer.js
+++ src/store/reducers/resultsReducer.js
@@ -4,10 +4,7 @@ import * as actionTypes from "../actiontypes";
 const resultsReducer = createReducer([], (builder) => {
   builder
     .addCase(actionTypes.STORE_RESULT, (state, action) => {
-      return [
-        ...state,
-        { id: new Date().toISOString(), value: action.payload },
-      ];
+      state.push({ id: new Date().toISOString(), value: action.payload });
     })
     .addCase(actionTypes.REMOVE_RESULT, (state, action) => {
       return state.filter((result) => result.id !== action.payload);
